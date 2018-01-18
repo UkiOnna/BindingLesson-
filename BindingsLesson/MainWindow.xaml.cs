60a26625475450518515a69e298beb2a1a8ac394
UkiOnna <sikirovtemirlan@mail.ru>
2018-01-18 17:53:25 +0600

    sdf

---
+++ BindingsLesson/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using BindingsLesson.Properties;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BindingsLesson
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<TrainTicket> tickets;
+        public MainWindow()
+        {
+            InitializeComponent();
+            tickets = new ObservableCollection<TrainTicket>
+            {
+                new TrainTicket
+                {
+                    Id=1,
+                    TrainNumber="1a",
+                    Price=10000,
+                    From="Astana",
+                    To="Almaty",
+                    DepartureDate=DateTime.Now,
+                    ArrivalDate=DateTime.Now.AddDays(7)
+                },
+
+        new TrainTicket
+                {
+                    Id=2,
+                    TrainNumber="2a",
+                    Price=20000,
+                    From="Astana",
+                    To="Moscow",
+                    DepartureDate=DateTime.Now,
+                    ArrivalDate=DateTime.Now.AddDays(7)
+                },
+
+    new TrainTicket
+                {
+                    Id=1,
+                    TrainNumber="3a",
+                    Price=30000,
+                    From="Aktobe",
+                    To="Almaty",
+                    DepartureDate=DateTime.Now,
+                    ArrivalDate=DateTime.Now.AddDays(7)
+                }
+};
+            DataContainer.ItemsSource = tickets;
+
+        }
+
+        private void DataAddingnewItem(object sender,AddingNewItemEventArgs e)
+        {
+            tickets[1].Id=100;
+        }
+    }
+}
---
+++ BindingsLesson/Properties/TrainTicket.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingsLesson.Properties
+{
+    public class TrainTicket : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int id;
+        //и остальные поля по свойствам
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+                //Тоже самое для других свойств
+            }
+        }
+        public string From { get; set; }
+        public string To { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public double Price { get; set; }
+        public string TrainNumber { get; set; }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
